bc074f3af6d4b4c5b0e6ba31cca798e4141c7a73
XuMeng <gosu@foxmail.com>
2018-03-19 15:17:02 +0800

    update

--- ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/service/UserServiceImpl.java
+++ ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.amonxu.zhuimeng.service.user.UserService;
  * Created by xumeng on 07/03/2018.
  */
 @Service(version = "1.0.0")
+@org.springframework.stereotype.Service
 public class UserServiceImpl implements UserService {
     @Override
     public String generateToken(String username, String password) {
