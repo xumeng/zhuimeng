ecd61b19a3cfc9f9b0fc85ad911f5c5282126752
XuMeng <gosu@foxmail.com>
2018-03-19 16:24:53 +0800

    update

--- ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/service/UserServiceImpl.java
+++ ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/service/UserServiceImpl.java
@@ -1,15 +1,13 @@
 package com.amonxu.zhuimeng.user.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import com.amonxu.zhuimeng.facade.user.UserService;
 
 /**
  * Created by xumeng on 07/03/2018.
  */
 @Service(version = "1.0.0")
 @org.springframework.stereotype.Service
-public class UserServiceImpl implements UserService {
-    @Override
+public class UserServiceImpl {
     public String generateToken(String username, String password) {
         return "HELLO MICRO SERVICE";
     }
