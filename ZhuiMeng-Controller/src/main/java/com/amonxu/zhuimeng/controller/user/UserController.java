3dc85a0a1b2a68b9b25216c475f78c73bb261eb2
XuMeng <gosu@foxmail.com>
2018-03-22 11:59:55 +0800

    update

--- ZhuiMeng-Controller/src/main/java/com/amonxu/zhuimeng/controller/user/UserController.java
+++ ZhuiMeng-Controller/src/main/java/com/amonxu/zhuimeng/controller/user/UserController.java
@@ -1,8 +1,11 @@
 package com.amonxu.zhuimeng.controller.user;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by xumeng on 07/03/2018.
  */
+@RestController
 public interface UserController {
     public String login(String username, String password);
 }
