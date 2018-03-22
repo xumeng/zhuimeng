84e6f4527c7dd2938f7cd57689f3758e2a10b0e6
XuMeng <gosu@foxmail.com>
2018-03-22 14:50:14 +0800

    update

--- ZhuiMeng-Controller/src/main/java/com/amonxu/zhuimeng/ZhuiMengControllerApplication.java
+++ ZhuiMeng-Controller/src/main/java/com/amonxu/zhuimeng/ZhuiMengControllerApplication.java
@@ -2,9 +2,18 @@ package com.amonxu.zhuimeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class ZhuiMengControllerApplication {
+public class ZhuiMengControllerApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ZhuiMengControllerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZhuiMengControllerApplication.class, args);
--- ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/ZhuiMengUserApplication.java
+++ ZhuiMeng-User/src/main/java/com/amonxu/zhuimeng/user/ZhuiMengUserApplication.java
@@ -2,13 +2,15 @@ package com.amonxu.zhuimeng.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZhuiMengUserApplication{
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(ZhuiMengUserApplication.class);
-//	}
+public class ZhuiMengUserApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ZhuiMengUserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZhuiMengUserApplication.class, args);
